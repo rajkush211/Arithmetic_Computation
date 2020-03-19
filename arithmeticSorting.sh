f11b9c751276eb0c07f570d403ecc75df907a69d
rajkush211 <rajkush211.rk@gmail.com>
2020-03-20 05:13:49 +0530

    ADDED: First operation performed

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -x
 
+#Problem Statement: This problem computes different arithmetic expressions and sorts the results.
+#Author: Raj Kush
+#Date: 20 March 2020
+
 clear
 
 read -p "Enter First value: " input1
 read -p "Enter Second value: " input2
 read -p "Enter Third value: " input3
 
+operation1=$(( input1 + input2 * input3 ))
+
 sleep 1
+
