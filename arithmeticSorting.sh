0cfeb0424cfb2c789769c150bb648bd85149d743
rajkush211 <rajkush211.rk@gmail.com>
2020-03-20 05:31:37 +0530

    ADDED: Fourth operation performed

--- arithmeticSorting.sh
+++ arithmeticSorting.sh
@@ -13,6 +13,7 @@ read -p "Enter Third value: " input3
 operation1=$(( input1 + input2 * input3 ))
 operation2=$(( input1 * input2 + input3 ))
 operation3=$(( input3 + input1 / input2 ))
+operation4=$(( input1 % input2 + input3 ))
 
 sleep 1
 
